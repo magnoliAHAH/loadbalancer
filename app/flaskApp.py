401879ba8d9a118d82fca1c1f023a46466ac2321
magnoliAHAH <137452290+magnoliAHAH@users.noreply.github.com>
2024-04-20 22:17:28 +0300

    Update flaskApp.py

--- app/flaskApp.py
+++ app/flaskApp.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Контейнер ID: {socket.gethostname()}"
+    return f"ID Контейнера: {socket.gethostname()}"
 
 if __name__ == "__main__":
     app.run(debug=True)
